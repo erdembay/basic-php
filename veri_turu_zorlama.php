bb9c9c6af57c5d28f2d1a36f25f9249c7997cebf
Erdem Bay <erdem.bay@mobilulasim.com>
2024-10-09 02:41:35 +0300

    veri dönüştürme implode explode çalışıldı

--- veri_turu_zorlama.php
+++ veri_turu_zorlama.php
@@ -1,7 +1,25 @@
 <?php
     declare(strict_types=1);
-    function topla(int|float $sayi1, int|float $sayi2): int|float {
-        return $sayi1 + $sayi2;
+    function topla(int|float $sayi1, int|float $sayi2): void {
+        $sonuc = $sayi1 + $sayi2;
     }
     echo topla(5.6, 10);
+    $name = '215';
+    $sayi = (int)($name);
+    $float_sayi = (float)($sayi);
+    $string_sayi = (string)($float_sayi);
+    var_dump($name);
+    echo '<br>';
+    var_dump($sayi);
+    echo '<br>';
+    var_dump($float_sayi);
+    echo '<br>';
+    var_dump($string_sayi);
+    echo '<br>';
+    $address = ['city' => 'Istanbul', 'country' => 'Turkey', 'continent' => 'Europe', 'planet' => 'Earth'];
+    $long_address = implode(' - ', $address);
+    echo $long_address;
+    echo '<br>';
+    $short_address = explode(' - ', $long_address);
+    print_r($short_address);
 ?>
